763d2f7922d6f7024913e3de82b3d17b557540d0
brian-wastle <brian.wastle1@gmail.com>
2023-08-02 10:01:54 -0400

    page generates hour blocks dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+let hourContainer = document.getElementById("hour-container");
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -21,3 +25,71 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+
+let localArray = {
+  9:'test9',
+  10:'test10',
+  11:'test11',
+  12:'test12',
+  1:'test1',
+  2:'test2',
+  3:'test3',
+  4:'test4',
+  5:'test5'
+}
+
+localStorage.setItem("calendarArray", JSON.stringify(localArray));
+
+
+let tempArray = JSON.parse(localStorage.getItem("calendarArray")) || [];
+
+
+//generate each row as an element
+for (let i = 0; i < Object.keys(tempArray).length; i++) {
+        
+  let hourBlock = document.createElement("div");
+  
+  
+  
+  
+  //get key name and append it to the string below
+  hourBlock.id = "hour-" + ;
+  hourBlock.className = "row time-block past";
+  
+  let hourBlock0 = document.createElement("div");
+  hourBlock0.className = "col-2 col-md-1 hour text-center py-3";
+
+  //get key name and append it to string below and use dayjs to convert time
+  hourBlock0.textContent = "9AM";
+  
+  let hourBlock1 = document.createElement("textArea");
+  hourBlock1.className = "col-8 col-md-10 description";
+  hourBlock1.setAttribute("rows", 3);
+  
+  
+  let hourBlock2 = document.createElement("button");
+  hourBlock2.className = "btn saveBtn col-2 col-md-1";
+  hourBlock2.setAttribute("aria-label", "save");
+
+  let hourBlock3 = document.createElement("i");
+  hourBlock3.className = "fas fa-save";
+  hourBlock3.setAttribute("aria-hidden", "true");
+
+  //hourBlock1.value = localStorage.getItem(tempArray[i]);
+  hourContainer.appendChild(hourBlock);
+  hourBlock.appendChild(hourBlock0);
+  hourBlock.appendChild(hourBlock1);
+  hourBlock.appendChild(hourBlock2);
+  hourBlock2.appendChild(hourBlock3);
+
+    };
+
+//compare time to current time to color code boxes
+
+//pull from local storage to populate hourly notes
+//document.querySelectorAll("textArea")[0].value = localStorage.getItem("");
+
+//when save button is saved
+//event delegation on button for storing hourly notes in localStorage
+//
